dce3865b05fe0b6e1db8e23f17dec498018d3f2f
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-19 10:28:39 +0100

    move + another refactorings

---
+++ a/b/A.java
@@ -0,0 +1,18 @@
+package a.b;
+
+public class A {
+	
+	public void m1() {
+		int aaa = 1;
+		int b = 2;
+
+		int c = aaa + b;
+
+		print(c);
+	}
+
+	public void print(int c) {
+		System.out.println(c);
+	}
+
+}
